ee1c792fa278341e4b3c567c84c01f84134c967e
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2023-12-13 15:10:50 -0800

    Create PythonTips.py
    some important tips in python

---
+++ Basic/PythonTips.py
@@ -0,0 +1,15 @@
+# Here you can find some important tips in Python
+
+# Tip 1:
+m = []
+for x in range(1, 11):
+  m.append(x**2)
+print m
+
+# The above code is equivalent with:
+m = [x**2 for x in range(1, 11)]
+
+'''
+Result:
+[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+'''
