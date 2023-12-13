a88b9c3854adfa3eb04c7e68df23cd83a60e7645
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2023-12-13 15:15:11 -0800

    Update PythonTips.py

--- Basic/PythonTips.py
+++ Basic/PythonTips.py
@@ -6,10 +6,10 @@ for x in range(1, 11):
   m.append(x**2)
 print m
 
-# The above code is equivalent with:
+# The above code is equivalent to:
 m = [x**2 for x in range(1, 11)]
 
-'''
+"""
 Result:
 [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
-'''
+"""
