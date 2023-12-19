9a2d95fac2351bf9ce7a54bc4157bb139bab450e
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2023-12-19 15:08:08 -0800

    Update PythonTips.py
    Using * in the parameter of a function

--- Basic/PythonTips.py
+++ Basic/PythonTips.py
@@ -13,3 +13,14 @@ m = [x**2 for x in range(1, 11)]
 Result:
 [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 """
+
+# Tip 2:
+
+''' using * means we don't know how many values will be passed to the function '''
+def calc_mean(first, *remainder)
+  mean = (first + sum(remainder)) / (1 + len(remainder))
+  print (mean)
+  return mean
+
+calc_mean(6, 10, 3, 5 , 2, 1)
+
