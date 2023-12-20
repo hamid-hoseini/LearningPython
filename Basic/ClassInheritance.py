7b5ac111957ff64bda073c99c9fb67fdf6c8aace
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2023-12-20 00:47:05 -0800

    Create ClassInheritance.py
    Class and Inheritance Exampl

---
+++ Basic/ClassInheritance.py
@@ -0,0 +1,39 @@
+class Patient(object):
+     ''' Medical centre patient'''
+     
+     status = 'patient'
+     
+     def __init__(self,name,age):
+         self.name = name
+         self.age = age
+         self.conditions = []
+         
+     def get_details(self):
+         print(f'Patient record: {self.name}, {self.age} years.' \
+               f' Current information: {self.conditions}.')
+         
+     def add_info(self,information):
+         self.conditions.append(information)
+
+
+steve = Patient('Steven Hughes',48)
+abigail = Patient('Abigail Sandwick',32)
+
+class Infant(Patient):
+    ''' Patient under 2 years'''
+    
+    def __init__(self,name,age):
+        self.vaccinations = []
+        super().__init__(name,age)
+        
+    def add_vac(self,vaccine):
+        self.vaccinations.append(vaccine)
+        
+    def get_details(self):
+         print(f'Patient record: {self.name}, {self.age} years.' \
+               f' Patient has had {self.vaccinations} vaccines.' \
+               f' Current information: {self.conditions}.' \
+               f'\n{self.name} IS AN INFANT, HAS HE HAD ALL HIS CHECKS?')
+        
+archie = Infant('Archie Fittleworth',0)        
+archie.add_vac('MMR') 
